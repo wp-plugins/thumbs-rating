95477fd9fb74a0e4b94d18cf8f58acca36809f60
quicoto <quicoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 08:44:14 +0000

    Oh crap!
    git-svn-id: https://plugins.svn.wordpress.org/thumbs-rating/tags/1.6@820835 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- thumbs-rating.php
+++ thumbs-rating.php
@@ -4,7 +4,7 @@ Plugin Name: Thumbs Rating
 Plugin URI: http://wordpress.org/plugins/thumbs-rating/
 Description: Add thumbs up/down rating to your content.
 Author: Ricard Torres
-Version: 1.5.1
+Version: 1.6
 Author URI: http://php.quicoto.com/
 */
 
